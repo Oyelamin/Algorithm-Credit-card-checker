e775c0f6cb8a2641959530efded9b7aca26786f3
Oyenxilaw <41972465+Oyelamin@users.noreply.github.com>
2019-09-25 09:38:41 +0100

    Add files via upload
    Uploading file

---
+++ check.py
@@ -0,0 +1,43 @@
+
+
+def luhn(card):
+   
+    second = 0
+    first = 0
+    nonTag = 0
+    cardObj = card.split()
+
+    for n in cardObj:
+        nonTag += (int(n[-1]) + int(n[-3]))
+        if len(n) == 4:
+            one = int(n[-2]) * 2 #-2
+            two = int(n[-4]) * 2  #-4
+            
+            if one >= 10:
+                one = str(one)
+                firstVal = int(one[0]) + int(one[1])
+                first += firstVal
+            elif one <= 9:
+                first += one
+
+            if two >= 10:
+                two = str(two)
+                secondVal = int(two[0]) + int(two[1])
+                second += secondVal
+              
+            elif two <= 9:
+                second += two
+        else:
+            return False
+
+    ftotal = first + second
+    total = ftotal + nonTag
+    total = str(int(total))
+    
+    if total[-1] == "0":
+        print(total)
+    else:
+        print(total)
+
+
+luhn("1566 5666 6663 6639")
